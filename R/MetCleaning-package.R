d9e3d4f2901f1de36442211876408aaf0b169ecb
jaspershen <shenxt1990@163.com>
2016-12-29 19:44:36 +0800

    change

---
+++ R/MetCleaning-package.R
@@ -0,0 +1,18 @@
+#' @name MetCleaning-package
+#' @aliases MetCleaning-package
+#' @docType package
+#' @title Data cleaning and statistical analysis of large scale MS-based data.
+#' @description Data cleaning and statistical analysis of large
+#' scale MS-based data.
+#' @details Package: MetCleaning
+#' @details Type: package
+#' @details Version: 0.99.0
+#' @details Date: 2016-12-29
+#' @details License: GPL (>= 2)
+#' @author Xiaotao Shen
+#' @author Maintainer: Xiaotao Shen shenxt@sioc.ac.cn
+#' @return A description of MetCleaning
+MetCleaning <- function(){
+  message(date(),
+          "\nData cleaning and statistical analysis of large scale MS-based data.")
+}
